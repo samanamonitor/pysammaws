b6ac6d6fff74e412d2bccecf701cf0069b5944b9
Fabian Baena <fabian.baena@samanagroup.com>
2025-02-21 16:10:42 -0500

    added log level setting

--- sammaws/query.py
+++ sammaws/query.py
@@ -49,7 +49,7 @@ class AwsQuery:
 		self._query_name = query_name
 		self._func = getattr(self.client, self._query_name)
 		self._object_name = self._service_queries[self._query_name]
-		self._log_level = kwargs.pop("log_level", "WARNING")
+		self._log_level = self._kwargs.pop("log_level", "WARNING")
 		log.setLevel(self._log_level)
 
 	def _get_data(self):
